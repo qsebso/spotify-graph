64e7092855a394bef37c1cf10f6623fda7c07645
unknown <quinnsebso@gmail.com>
2024-10-06 14:52:49 -0500

    change page order

--- observablehq.config.js
+++ observablehq.config.js
@@ -6,15 +6,12 @@ export default {
   // The pages and sections in the sidebar. If you don’t specify this option,
   // all pages will be listed in alphabetical order. Listing pages explicitly
   // lets you organize them into sections and have unlisted pages.
-  // pages: [
-  //   {
-  //     name: "Examples",
-  //     pages: [
-  //       {name: "Dashboard", path: "/example-dashboard"},
-  //       {name: "Report", path: "/example-report"}
-  //     ]
-  //   }
-  // ],
+   // The pages and sections in the sidebar.
+   pages: [
+    { name: "Weekly Top 10!", path: "/weekly-hits" },
+    { name: "Artist & Genre Recommendations", path: "/sunburst-diagram" },
+    { name: "How do I get my Data?", path: "/help-page" }
+  ],
 
   // Content to add to the head of the page, e.g. for a favicon:
   head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
