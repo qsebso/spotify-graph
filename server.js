739c971d5693ce48db89354b3ba461886eedc3e8
unknown <quinnsebso@gmail.com>
2024-10-06 00:34:07 -0500

    added acsess to genre file

--- server.js
+++ server.js
@@ -569,6 +569,20 @@ app.get('/sample_cumulative_for_barchart', (req, res) => {
   });
 });
 
+// Route to the artist by genre
+app.get('/artists_by_genre', (req, res) => {
+  const filePath = path.join(__dirname, 'artists_by_genre.json');
+
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.error('File not found:', filePath); // Log error
+      return res.status(404).send('File not found');
+    }
+
+    res.sendFile(filePath);
+  });
+});
+
 // Route to access uploaded files
 app.get('/uploads/:filename', (req, res) => {
   const filepath = path.join(__dirname, 'uploads', req.params.filename);
